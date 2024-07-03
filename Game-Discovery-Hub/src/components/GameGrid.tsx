364668ae9fc72f1d24865174856672875d7e0517
Raahim Nadeem <raahimnadeem@gosaas.io>
2024-07-03 17:09:58 +0500

    Create a custom hook to fetch the games

--- Game-Discovery-Hub/src/components/GameGrid.tsx
+++ Game-Discovery-Hub/src/components/GameGrid.tsx
@@ -1,39 +1,18 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-const [games, setGames] = useState<Game[]>([]); // Start with an empty array
-  const [error, setError] = useState("");
-
-  // Effect hook to send the fetech requests to the back-end
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results)
-        console.log(res.data.results);
-      })
-        .catch((err) => setError(err.message));
-  }, []); // Empty dependency array: Run only once on mount
-  
-
+    const {games, error} = useGames();
   return (
-    <ul>
-      {games?.map(game => 
-        <li key={game.id}>{game.name}</li>
-    )}
-    </ul>
-  )
-}
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games?.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default GameGrid;
