434890d43c4ce11d5c54329f185bf3edf6d474f2
Raahim Nadeem <raahim.nade@gmail.com>
2024-07-04 12:08:45 +0500

    Show Spinner While Fetching Genres

--- Game-Discovery-Hub/src/components/GenreList.tsx
+++ Game-Discovery-Hub/src/components/GenreList.tsx
@@ -1,19 +1,29 @@
-import { HStack, List, ListItem, Image, Text} from '@chakra-ui/react';
-import useGenres from '../hooks/useGenres';
-import getCroppedImageUrl from '../services/image-url';
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const {data} = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
   return (
     <List>
-        {data.map(genre => <ListItem paddingY={1.5} key={genre.id}>
-            <HStack>
-                <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                <Text fontSize={'lg'}>{genre.name}</Text> 
-            </HStack>
-        </ListItem>)}
+      {data.map((genre) => (
+        <ListItem paddingY={1.5} key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
-  )
-}
+  );
+};
 
-export default GenreList
+export default GenreList;
