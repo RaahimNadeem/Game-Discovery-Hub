6a5a1b3f1bdcf6a1a52bcb45a5fa874efa1165b1
Raahim Nadeem <raahim.nade@gmail.com>
2024-07-04 11:52:33 +0500

    Create a Generic Data Fetching Hook

---
+++ Game-Discovery-Hub/src/hooks/useData.ts
@@ -0,0 +1,40 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import GenreList from "../components/GenreList";
+
+
+interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+    const [data, setData] = useState<T[]>([])
+    const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
+// Effect hook to send the fetech requests to the back-end
+  useEffect(() => {
+    // handling cancellations
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading};
+}
+
+export default useData;
--- Game-Discovery-Hub/src/hooks/useGames.ts
+++ Game-Discovery-Hub/src/hooks/useGames.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
@@ -16,37 +17,6 @@ export interface Game {
   metacritic: number;
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]); // Start with an empty array
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  // Effect hook to send the fetech requests to the back-end
-  useEffect(() => {
-    // handling cancellations
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading};
-};
-
+const useGames = () => useData<Game>('/games');
 export default useGames;
