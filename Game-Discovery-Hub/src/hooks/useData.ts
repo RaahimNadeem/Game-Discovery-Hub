2a808cf9d33795ab7b8b7546d68b9c88fffffde8
Raahim Nadeem <raahim.nade@gmail.com>
2024-07-04 12:50:04 +0500

    Build Platform Selector

--- Game-Discovery-Hub/src/hooks/useData.ts
+++ Game-Discovery-Hub/src/hooks/useData.ts
@@ -1,7 +1,6 @@
 import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import GenreList from "../components/GenreList";
 
 
 interface FetchResponse<T> {
