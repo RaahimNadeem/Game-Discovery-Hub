5ff2988b877a4881cb1889dc06b549dd967914eb
Raahim Nadeem <raahimnadeem@gosaas.io>
2024-07-04 10:16:55 +0500

    Display Critic Score

--- Game-Discovery-Hub/src/components/GameCard.tsx
+++ Game-Discovery-Hub/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -9,13 +10,18 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image} />
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}></PlatformIconList>
-        </CardBody>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent={'space-between'}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          ></PlatformIconList>
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
+  );
 };
 
 export default GameCard;
